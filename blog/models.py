b190df5df9d8d8e14ae693c147e0b078dbd46bd0
unknown <you@example.com>
2020-04-24 20:50:34 +0530

    first commit

--- blog/models.py
+++ blog/models.py
@@ -28,7 +28,7 @@ class Post(models.Model):
     s_heading = models.CharField(max_length=2000, default='')
     s_heading_text = models.CharField(max_length=2000, default='')
     category = models.CharField(max_length=2000, choices=CATEGORY_CHOICES, default='')
-    created_date = models.DateTimeField()
+    created_date = models.DateTimeField(null = True, blank = True, default= timezone.now)
     
     tag_1 = models.CharField(max_length=2000, default='')
     tag_2 = models.CharField(max_length=2000, default='')
